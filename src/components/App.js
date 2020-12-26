e08bfd21b19cbffeb9d074b6dd671659134633c7
thomasrosen <thomas.rosen@me.com>
2020-12-26 05:04:04 +0100

    imported Ubuntu fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import './App.css'
 import Timeline from './Timeline.js'
 
+import '../fonts/ubuntu-v15-latin/index.css'
+import '../fonts/ubuntu-mono-v10-latin/index.css'
+
 function App() {
   return (
   	<Timeline />
