1f532ab1aa1324fcde65a1cb2cb3d68e32149c7c
thomasrosen <thomas.rosen@me.com>
2021-01-05 20:06:49 +0100

    FOGOTTEN import the tilt-shift code (vanilla-tilt)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Timeline from './Timeline.js'
 import '../fonts/ubuntu-v15-latin/index.css'
 import '../fonts/ubuntu-mono-v10-latin/index.css'
 
+import 'vanilla-tilt'
+
 function App() {
   return (
   	<Timeline />
