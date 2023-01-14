46bc6bde3a7474f6992f89b09696f89c65677e84
thomasrosen <thomas.rosen@me.com>
2023-01-14 12:30:50 +0100

    moved the header to react

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
 import './App.css'
-// import Timeline from './Timeline.js'
 
 import '../fonts/ubuntu-v15-latin/index.css'
 import '../fonts/ubuntu-mono-v10-latin/index.css'
 
-import 'vanilla-tilt'
-
 function App() {
-  return null
-  // return (
-  // 	<Timeline />
-  // )
+  return <>
+  
+    <center>
+      <br />
+      <br />
+
+      <div alt="Thomas Rosen eating a slice watermelon." class="headerImageOfMe"></div>
+      <h1>Thomas Rosen</h1>
+
+      <br />
+      <br />
+    
+      <p style={{ maxWidth: '300px' }}>
+        Hi, nice to meet you 👋<br />
+        <br />
+        <a href="mailto:hello@thomasrosen.me">Send me a text</a> if you have any questions or whatever else.<br />I'd love to hear from you!<br />
+        <br />
+        Best, Thomas
+      </p>
+
+      <br />
+      <br />
+    </center>
+    
+  </>
 }
 
 export default App
