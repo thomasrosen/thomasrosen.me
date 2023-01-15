a3c41ed8d920faea27405642d8f961e5aa49e2f9
thomasrosen <thomas.rosen@me.com>
2023-01-15 01:03:03 +0100

    added a quote about me

--- src/components/pages/welcome.js
+++ src/components/pages/welcome.js
@@ -3,6 +3,8 @@ export default function Welcome() {
     <p style={{ maxWidth: '300px' }}>
       Hi, nice to meet you 👋<br />
       <br />
+      For me, writing software – coding – has always been a tool that helps me achieve what I want to do. And that is not developing software per se, but to help everyone be more conscious of each other and more compassionate.
+      <br />
       <a href="mailto:hello@thomasrosen.me">Send me a text</a> if you have any questions or whatever else.<br />I'd love to hear from you!<br />
       <br />
       Best, Thomas
