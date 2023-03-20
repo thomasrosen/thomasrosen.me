3acb42dc780865593893e95e6d68c7034c11f83f
thomasrosen <thomas.rosen@me.com>
2023-03-20 06:40:44 +0100

    fix: plaintext

--- feed_generator.js
+++ feed_generator.js
@@ -140,7 +140,7 @@ function generate_rss_feed(options) {
 
     feed.item({
       title: article.data.title,
-      description: article.data.plaintext,
+      description: article.plaintext,
       url: `https://thomasrosen.me/articles/${article.data.slug}`, // link to the item
       guid: article.data.id || undefined, // optional - defaults to url
       categories: article.data.tags, // optional - array of item categories
@@ -158,8 +158,8 @@ function generate_rss_feed(options) {
         { 'itunes:episode': episode_number },
         { 'itunes:season': 1 },
         { 'itunes:author': author },
-        { 'itunes:subtitle': article.data.plaintext },
-        { 'itunes:summary': article.data.plaintext },
+        { 'itunes:subtitle': article.plaintext },
+        { 'itunes:summary': article.plaintext },
         { 'itunes:image': {
           _attr: {
             href: coverphoto_url
