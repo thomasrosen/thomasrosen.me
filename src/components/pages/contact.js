da184da76f8cd8b3af179f2ad0248243c05c7e24
thomasrosen <thomas.rosen@me.com>
2023-05-18 11:50:46 +0200

    moved umami to version 2

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
         <Dot color="var(--primary-color)" />
         Email
       </h3>
-      <a href="mailto:hello@thomasrosen.me" target="_blank" rel="noreferrer" className="umami--click--Email">
+      <a href="mailto:hello@thomasrosen.me" target="_blank" rel="noreferrer" data-umami-event="Email">
         hello@thomasrosen.me
       </a>
 
@@ -19,7 +19,7 @@ export default function Contact() {
         <Dot color="#502379" />
         Volt Email
       </h3>
-      <a href="mailto:thomas.rosen@volteuropa.org" target="_blank" rel="noreferrer" className="umami--click--Volt Email">
+      <a href="mailto:thomas.rosen@volteuropa.org" target="_blank" rel="noreferrer" data-umami-event="Volt Email">
         thomas.rosen@volteuropa.org
       </a>
 
@@ -30,7 +30,7 @@ export default function Contact() {
 
 /*
   <li>
-    <a href="mailto:thomas.rosen@qiekub.org" target="_blank" rel="nofollow" className="umami--click--Email">
+    <a href="mailto:thomas.rosen@qiekub.org" target="_blank" rel="nofollow" data-umami-event="Email">
       <span className="icon" data-icon="Old Email" style="color: #00695c;"></span>
       <span className="text">thomas.rosen@qiekub.org</span>
     </a>
--- src/components/pages/press.js
+++ src/components/pages/press.js
@@ -8,7 +8,7 @@ export default function Press() {
       <div>🎞</div>
       <div>
         <h3 className="no_flex">
-          YouTube Video in German with Benjamin Körner: <a href="https://www.youtube.com/watch?v=8IbdHXZV7sE" target="_blank" rel="noreferrer" className="umami--click--article">
+          YouTube Video in German with Benjamin Körner: <a href="https://www.youtube.com/watch?v=8IbdHXZV7sE" target="_blank" rel="noreferrer" data-umami-event="article">
             Volt Partei reagiert auf Rezo Zerstörung FINALE: Korruption
           </a>
         </h3>
@@ -20,7 +20,7 @@ export default function Press() {
       <div>📄</div>
       <div>
         <h3 className="no_flex">
-          Article with 🎶 Audio Version: <a href="https://www.esquiremag.ph/culture/tech/mapbeks-lgbtq-a00225-20210720" target="_blank" rel="noreferrer" className="umami--click--article">
+          Article with 🎶 Audio Version: <a href="https://www.esquiremag.ph/culture/tech/mapbeks-lgbtq-a00225-20210720" target="_blank" rel="noreferrer" data-umami-event="article">
             These Maps Show Every LGBTQI+ Safe Space, HIV Facility, and Story in the Philippines
           </a>
         </h3>
@@ -32,7 +32,7 @@ export default function Press() {
       <div>📄</div>
       <div>
         <h3 className="no_flex">
-          Article: <a href="https://www.aboutamazon.com/news/aws/mapping-safe-places-for-people-in-the-lgbtq-community" target="_blank" rel="noreferrer" className="umami--click--article">
+          Article: <a href="https://www.aboutamazon.com/news/aws/mapping-safe-places-for-people-in-the-lgbtq-community" target="_blank" rel="noreferrer" data-umami-event="article">
             Mapping safe places for people in the LGBTQ+ community
           </a>
         </h3>
@@ -44,7 +44,7 @@ export default function Press() {
       <div>📄</div>
       <div>
         <h3 className="no_flex">
-          PDF: <a href="https://www.qiekub.org/files/Ausarbeitung_Freimanis_Mueller_Reitmann­_Weidinger.pdf" target="_blank" rel="noreferrer" className="umami--click--article">
+          PDF: <a href="https://www.qiekub.org/files/Ausarbeitung_Freimanis_Mueller_Reitmann­_Weidinger.pdf" target="_blank" rel="noreferrer" data-umami-event="article">
             Prozess, Methoden und Ergebnisse eines Forschungsprojektes zum Thema LGBTQ+-Mapping
           </a>
         </h3>
@@ -56,7 +56,7 @@ export default function Press() {
       <div>📄</div>
       <div>
         <h3 className="no_flex">
-          Article: <a href="https://wonder.ph/life/tech/how-you-can-put-lgbtqia-safe-spaces-on-the-map/" target="_blank" rel="noreferrer" className="umami--click--article">
+          Article: <a href="https://wonder.ph/life/tech/how-you-can-put-lgbtqia-safe-spaces-on-the-map/" target="_blank" rel="noreferrer" data-umami-event="article">
             How You Can Put LGBTQIA+ Safe Spaces on the Map
           </a>
         </h3>
@@ -68,7 +68,7 @@ export default function Press() {
       <div>🎞</div>
       <div>
         <h3 className="no_flex">
-          Live Workshop on Facebook: <a href="https://www.facebook.com/mapbeks/videos/239256370717457" target="_blank" rel="noreferrer" className="umami--click--article">
+          Live Workshop on Facebook: <a href="https://www.facebook.com/mapbeks/videos/239256370717457" target="_blank" rel="noreferrer" data-umami-event="article">
             SOGIESC 101 and LGBT Safe Spaces Mapping Training with MapBeks, QueerMap by qiekub, Rotaract Club of Southern City Colleges, Pantay, Ministry of Mapping, Wonder and PUP GLITCH!!!
           </a>
         </h3>
