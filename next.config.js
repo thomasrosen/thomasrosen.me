030309e11787fd7a458352d215fe26c38c957a0f
thomasrosen <thomas.rosen@me.com>
2023-11-18 01:00:56 +0000

    thomasrosen published a site update

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const nextConfig = {
   output: 'export',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
+  skipTrailingSlashRedirect: false,
 
   // Optional: Change the output directory `out` -> `dist`
   distDir: 'build',
