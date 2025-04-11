54533a0056ae2f8f03231568af80293b972d1114
Thomas Rosen <github@thomasrosen.me>
2025-04-11 05:18:10 +0200

    Update getRandomVars.js

--- src/utils/getRandomVars.js
+++ src/utils/getRandomVars.js
@@ -4,7 +4,7 @@ export function getRandomVars() {
     '--scale-start': `${Math.random() * 1 + 1}`, // 1 to 2
     '--scale-end': `${Math.random() * 1 + 2}`, // 2 to 3
     '--opacity-start': `${Math.random() * 0.5 + 0.5}`, // 0.5 to 1
-    '--opacity-end': `${Math.random() * 0.75 + 0.25}`, // 0.25 to 1
+    '--opacity-end': `${Math.random() * 1 + 0}`, // 0 to 1
     '--duration': `${Math.random() * 20 + 10}s`, // 10s to 30s
   }
 }
