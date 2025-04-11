f8099aa061f6b46130115ba1bd5562e4af84a854
Thomas Rosen <github@thomasrosen.me>
2025-04-11 03:38:19 +0200

    update to next15

--- src/app/playlists/[id]/page.jsx
+++ src/app/playlists/[id]/page.jsx
@@ -1,8 +1,8 @@
-import { Dot } from '@/components/Dot';
-import { Emoji } from '@/components/Emoji';
-import { loadPlaylists } from '@/utils/loadPlaylists';
-import fs from 'fs';
-import React from 'react';
+import { Dot } from '@/components/Dot'
+import { Emoji } from '@/components/Emoji'
+import { loadPlaylists } from '@/utils/loadPlaylists'
+import fs from 'fs'
+import React from 'react'
 
 function get_genres(playlist) {
   if (!playlist || !playlist.Songs || Array.isArray(playlist.Songs) === false) {
@@ -32,14 +32,18 @@ function get_genres(playlist) {
 export function generateStaticParams() {
   const playlists = loadPlaylists()
 
-  return playlists.flatMap(playlist => ([
-    {id: playlist.name},
-    // {id: encodeURIComponent(playlist.name)}, // this is needed for dev
-  ]))
+  return playlists.flatMap((playlist) => [
+    {
+      id:
+        process.env.NODE_ENV === 'production'
+          ? playlist.name
+          : encodeURIComponent(playlist.name),
+    },
+  ])
 }
 
-export default function Page({ params }) {
-  let { id } = params || {}
+export default async function Page({ params }) {
+  let { id } = (await params) || {}
 
   if (!id) {
     throw new Error('No playlist id provided.')
@@ -57,136 +61,206 @@ export default function Page({ params }) {
   }
 
   if (
-    typeof playlist.Songs === 'object'
-    && playlist.Songs !== null
-    && !Array.isArray(playlist.Songs)
+    typeof playlist.Songs === 'object' &&
+    playlist.Songs !== null &&
+    !Array.isArray(playlist.Songs)
   ) {
     playlist.Songs = [playlist.Songs]
   }
 
-  const date_month = (
+  const date_month =
     typeof playlist?.Date === 'string' && playlist?.Date?.length > 0
       ? playlist?.Date
       : ''
-  )
   const genres = get_genres(playlist)
   const song_count = playlist?.Songs?.length
 
-  return <div className={`tab_content article ${!!playlist && playlist.font === 'serif' ? 'serif_font' : 'sans_serif_font'}`}>
-
-    {
-      !!playlist
-        ? <>
-
-          <h2 itemProp="headline">
-            <time dateTime={date_month} title={date_month} itemProp="datePublished">{playlist.Name}</time>
+  return (
+    <div
+      className={`tab_content article ${
+        !!playlist && playlist.font === 'serif'
+          ? 'serif_font'
+          : 'sans_serif_font'
+      }`}
+    >
+      {!!playlist ? (
+        <>
+          <h2 itemProp='headline'>
+            <time
+              dateTime={date_month}
+              title={date_month}
+              itemProp='datePublished'
+            >
+              {playlist.Name}
+            </time>
           </h2>
 
-          <p><strong>
-            <span className="tag_row" itemProp="keywords">
-              {genres.map(genre => <button className="small" disabled key={genre}>{genre}</button>)}
-              <br />
-              <span>{song_count === 1 ? 'One Song' : `${song_count} Songs`}</span>
-            </span>
-          </strong></p>
+          <p>
+            <strong>
+              <span className='tag_row' itemProp='keywords'>
+                {genres.map((genre) => (
+                  <button className='small' disabled key={genre}>
+                    {genre}
+                  </button>
+                ))}
+                <br />
+                <span>
+                  {song_count === 1 ? 'One Song' : `${song_count} Songs`}
+                </span>
+              </span>
+            </strong>
+          </p>
 
           <hr />
 
-          <div className="links_grid" style={{
-            gridTemplateColumns: 'auto'
-          }}>
-            {song_count > 0 && playlist.Songs.map((song, index) => {
+          <div
+            className='links_grid'
+            style={{
+              gridTemplateColumns: 'auto',
+            }}
+          >
+            {song_count > 0 &&
+              playlist.Songs.map((song, index) => {
+                const album_artwork = song['Album Artwork']
+                const title = song.Title
+                const genre = song.Genre
+                const url = song['Store URL']
+                const play_count = song['Play Count']
 
-              const album_artwork = song['Album Artwork']
-              const title = song.Title
-              const genre = song.Genre
-              const url = song['Store URL']
-              const play_count = song['Play Count']
-
-              return <div
-                key={index}
-                className="links_grid_item color_inherit"
-              >
-
-                <a
-                  href={url}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="inherit_color"
-                >
-                  <div className="image_container" style={{
-                    width: '64px',
-                    height: '64px',
-                  }}>
-                    {
-                      typeof album_artwork === 'string' && album_artwork.length > 0
-                        ? <img src={album_artwork} alt={title} style={{
-                              filter: 'contrast(1.1) saturate(1.2)',
-                            }} />
-                        : <div></div>
-                    }
-                  </div>
-                </a>
-
-                <div>
-                  <a
-                    href={url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="inherit_color"
+                const imageComp = (
+                  <div
+                    className='image_container'
+                    style={{
+                      width: '64px',
+                      height: '64px',
+                      position: 'relative',
+                    }}
                   >
-                    <h3 className="big" style={{ marginBlockStart: '0' }}>
-                      {title}
-                    </h3>
-                  </a>
+                    {typeof album_artwork === 'string' &&
+                    album_artwork.length > 0 ? (
+                      <img
+                        src={album_artwork}
+                        alt={title}
+                        style={{
+                          filter: 'contrast(1.1) saturate(1.2)',
+                          margin: '0',
+                          zIndex: '10',
+                        }}
+                      />
+                    ) : (
+                      <div />
+                    )}
+                    <div
+                      className='image_backdrop_glow'
+                      style={{
+                        '--background-image': `url(${album_artwork})`,
+                      }}
+                    />
+                  </div>
+                )
 
-                  <p>
-                    {
-                      typeof song.Artist === 'string' && song.Artist.length > 0
-                        ? <strong>{song.Artist}</strong>
-                        : null
-                    }
-                    {
-                      typeof song.Album === 'string' && song.Album.length > 0
-                        ? (
-                          typeof song.Artist === 'string' && song.Artist.length > 0
+                const headlineComp = (
+                  <h3 className='big' style={{ marginBlockStart: '0' }}>
+                    {title}
+                  </h3>
+                )
+
+                return (
+                  <div
+                    key={index}
+                    className='links_grid_item color_inherit'
+                    style={{
+                      zIndex: '20',
+                    }}
+                  >
+                    {url ? (
+                      <a
+                        href={url}
+                        target='_blank'
+                        rel='noreferrer'
+                        className='inherit_color'
+                      >
+                        {imageComp}
+                      </a>
+                    ) : (
+                      imageComp
+                    )}
+
+                    <div>
+                      {url ? (
+                        <a
+                          href={url}
+                          target='_blank'
+                          rel='noreferrer'
+                          className='inherit_color'
+                        >
+                          {headlineComp}
+                        </a>
+                      ) : (
+                        headlineComp
+                      )}
+
+                      <p>
+                        {typeof song.Artist === 'string' &&
+                        song.Artist.length > 0 ? (
+                          <strong>{song.Artist}</strong>
+                        ) : null}
+                        {typeof song.Album === 'string' && song.Album.length > 0
+                          ? typeof song.Artist === 'string' &&
+                            song.Artist.length > 0
                             ? `: ${song.Album}`
                             : song.Album
-                        )
-                        : null
-                    }
-                  </p>
-
-                  <p><strong style={{
-                    display: 'inline-flex',
-                    alignItems: 'center',
-                    gap: '5px 15px',
-                    flexWrap: 'wrap',
-                  }}>
-                    {
-                      [
-                        (
-                          typeof genre === 'string' && genre.length > 0
-                            ? <span className="tag_row" title={`Genre: ${genre}`}>
-                              <button className="small" disabled key={genre}>{genre}</button>
-                            </span>
-                            : null
-                        ),
-                        <time title={`Duration: ${song.Duration} min`}>{song.Duration} min</time>,
-                        (
-                          play_count > 0
-                            ? <Emoji title={`Play Count: ${play_count}`}>🔄 {play_count}</Emoji>
-                            : null
-                        ),
-                        song['Is Explicit'] === '1' ? <Emoji title="Song is Explicit" alt="Song is Explicit">🔥</Emoji> : null,
-                      ]
-                        .filter(Boolean)
-                        .map((item, index) => <React.Fragment key={index}>{item}</React.Fragment>)
-                    }
-                  </strong></p>
-                </div>
-              </div>
-            })}
+                          : null}
+                      </p>
+                      <p>
+                        <strong
+                          style={{
+                            display: 'inline-flex',
+                            alignItems: 'center',
+                            gap: '5px 15px',
+                            flexWrap: 'wrap',
+                          }}
+                        >
+                          {[
+                            typeof genre === 'string' && genre.length > 0 ? (
+                              <span
+                                className='tag_row'
+                                title={`Genre: ${genre}`}
+                              >
+                                <button className='small' disabled key={genre}>
+                                  {genre}
+                                </button>
+                              </span>
+                            ) : null,
+                            <time title={`Duration: ${song.Duration} min`}>
+                              {song.Duration} min
+                            </time>,
+                            play_count > 0 ? (
+                              <Emoji title={`Play Count: ${play_count}`}>
+                                🔄 {play_count}
+                              </Emoji>
+                            ) : null,
+                            song['Is Explicit'] === '1' ? (
+                              <Emoji
+                                title='Song is Explicit'
+                                alt='Song is Explicit'
+                              >
+                                🔥
+                              </Emoji>
+                            ) : null,
+                          ]
+                            .filter(Boolean)
+                            .map((item, index) => (
+                              <React.Fragment key={index}>
+                                {item}
+                              </React.Fragment>
+                            ))}
+                        </strong>
+                      </p>
+                    </div>
+                  </div>
+                )
+              })}
           </div>
 
           <br />
@@ -194,9 +268,8 @@ export default function Page({ params }) {
           <center>
             <Dot />
           </center>
-
         </>
-        : null
-    }
-  </div>
+      ) : null}
+    </div>
+  )
 }
