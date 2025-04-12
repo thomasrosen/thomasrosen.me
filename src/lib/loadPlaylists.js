ec75972a934ca9aaae439bdf30f28ac72171663d
Thomas Rosen <github@thomasrosen.me>
2025-04-12 02:36:14 +0200

    sort by date

--- src/lib/loadPlaylists.js
+++ src/lib/loadPlaylists.js
@@ -2,9 +2,9 @@ import playlistData from '@/data/music/playlists.json'
 
 export function loadPlaylists() {
   try {
-    const playlists = playlistData.playlists.reverse()
-
-    return playlists
+    return playlistData.playlists.sort(
+      (a, b) => new Date(b.date_month) - new Date(a.date_month)
+    )
   } catch (error) {
     console.error('ERROR_re4P6nv6', error)
   }
