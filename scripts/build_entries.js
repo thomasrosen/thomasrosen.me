b9c5eab6a351e7e47f4443508bdc20c70482ca1d
Thomas Rosen <github@thomasrosen.me>
2025-04-12 06:58:08 +0200

    fix entries build script

--- scripts/build_entries.js
+++ scripts/build_entries.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const matter = require('gray-matter');
-
+const path = require('path');
 // Read YAML file
-const yamlContent = fs.readFileSync('./data_about_thomasrosen/entries.yml', 'utf8');
+const fullpath_load = path.join(__dirname, '../data_about_thomasrosen/entries.yml');
+const yamlContent = fs.readFileSync(fullpath_load, 'utf8');
 const { data: entries } = matter(`---
 ${yamlContent}
 ---`);
 
 // Write JSON file
-fs.writeFileSync('./src/data/entries.json', JSON.stringify(entries, null, 2));
+const fullpath_save = path.join(__dirname, '../src/data/entries.json');
+fs.writeFileSync(fullpath_save, JSON.stringify(entries, null, 2));
 
 console.log('✅ Successfully converted entries.yml to entries.json'); 
