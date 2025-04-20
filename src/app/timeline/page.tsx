9fcc50c45e7cc0241700a96301b2aa775bd377ae
Thomas Rosen <github@thomasrosen.me>
2025-04-20 10:48:21 +0200

    Update page.tsx

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -3,6 +3,7 @@ import timelineData from '@/data/timeline/entries.json'
 import '@/fonts/petrona-v28-latin/index.css'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import React from 'react'
 
 type Entry = {
   date: string
@@ -19,7 +20,26 @@ type Entry = {
   }
 }
 
-function EntryTextContent({ entry }: { entry: Entry }) {
+function getMonthName(monthIndex: number) {
+  // Use a fixed locale and options to ensure consistent rendering
+  const options = { month: 'long' } as const
+  return new Date(2000, monthIndex, 1).toLocaleString('en-US', options)
+}
+
+function EntryTextContent({
+  entryBefore,
+  entry,
+  entryAfter,
+}: {
+  entryBefore?: Entry
+  entry: Entry
+  entryAfter?: Entry
+}) {
+  const sameDayAsBefore =
+    entryBefore &&
+    new Date(entryBefore.date).toDateString() ===
+      new Date(entry.date).toDateString()
+
   return (
     <>
       {entry.date && (
@@ -29,15 +49,22 @@ function EntryTextContent({ entry }: { entry: Entry }) {
           className='flex items-center gap-2 font-normal text-xs'
         >
           <span className='opacity-60 shrink-0'>
-            {new Date(entry.date).toLocaleDateString('de-DE', {
-              month: 'long',
-              day: 'numeric',
-              year: 'numeric',
-              hour: '2-digit',
-              minute: '2-digit',
-            })}
+            {sameDayAsBefore
+              ? new Date(entry.date).toLocaleTimeString('de-DE', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })
+              : new Date(entry.date).toLocaleDateString('de-DE', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
           </span>
-          <div className='h-[1px] bg-foreground w-full opacity-50' />
+          {sameDayAsBefore ? null : (
+            <div className='h-[1px] bg-foreground w-full opacity-50' />
+          )}
         </Typo>
       )}
       {entry.title && (
@@ -54,10 +81,24 @@ function EntryTextContent({ entry }: { entry: Entry }) {
   )
 }
 
-function EntryText({ entry, className }: { entry: Entry; className?: string }) {
+function EntryText({
+  entry,
+  className,
+  entryBefore,
+  entryAfter,
+}: {
+  entry: Entry
+  className?: string
+  entryBefore?: Entry
+  entryAfter?: Entry
+}) {
   return (
-    <div className={cn('my-4 flex flex-col gap-2', className)}>
-      <EntryTextContent entry={entry} />
+    <div className={cn('mb-4 flex flex-col gap-2', className)}>
+      <EntryTextContent
+        entry={entry}
+        entryBefore={entryBefore}
+        entryAfter={entryAfter}
+      />
     </div>
   )
 }
@@ -66,10 +107,14 @@ function EntryImage({
   entry,
   isFirstImage = false,
   className = '',
+  entryBefore,
+  entryAfter,
 }: {
   entry: Entry
   isFirstImage?: boolean
   className?: string
+  entryBefore?: Entry
+  entryAfter?: Entry
 }) {
   if (!entry.image || typeof entry.image !== 'string') {
     return null
@@ -110,21 +155,39 @@ function EntryImage({
         <div className='z-50 absolute bottom-0 left-0 right-0 h-[calc(100%-96px)] backdrop-blur-[32px] mask-t-from-[calc(100%-64px)]' />
         <div className='z-50 absolute bottom-0 left-0 right-0 h-[calc(100%-32px)] mask-t-to-[100%] bg-background opacity-30' />
         <div className='relative z-60 text-foreground flex flex-col gap-2 p-4'>
-          <EntryTextContent entry={entry} />
+          <EntryTextContent
+            entry={entry}
+            entryBefore={entryBefore}
+            entryAfter={entryAfter}
+          />
         </div>
       </div>
     </div>
   )
 }
 
-function EntryLink({ entry, className }: { entry: Entry; className?: string }) {
+function EntryLink({
+  entry,
+  className,
+  entryBefore,
+  entryAfter,
+}: {
+  entry: Entry
+  className?: string
+  entryBefore?: Entry
+  entryAfter?: Entry
+}) {
   if (!entry.url) {
     return null
   }
 
   return (
-    <div className={cn('my-4 flex flex-col gap-2', className)}>
-      <EntryTextContent entry={entry} />
+    <div className={cn('mb-4 flex flex-col gap-2', className)}>
+      <EntryTextContent
+        entry={entry}
+        entryBefore={entryBefore}
+        entryAfter={entryAfter}
+      />
 
       <a href={entry.url} target='_blank' rel='noreferrer' className='text-sm'>
         {entry.title || entry.url}
@@ -133,20 +196,18 @@ function EntryLink({ entry, className }: { entry: Entry; className?: string }) {
   )
 }
 
-function getMonthName(monthIndex: number) {
-  // Use a fixed locale and options to ensure consistent rendering
-  const options = { month: 'long' } as const
-  return new Date(2000, monthIndex, 1).toLocaleString('en-US', options)
-}
-
 function Entry({
   entry,
   isFirstImage = false,
   className = '',
+  entryBefore,
+  entryAfter,
 }: {
   entry: Entry
   isFirstImage?: boolean
   className?: string
+  entryBefore?: Entry
+  entryAfter?: Entry
 }) {
   const parsedEntry = {
     ...entry,
@@ -155,15 +216,31 @@ function Entry({
 
   switch (parsedEntry.displayAs) {
     case 'text':
-      return <EntryText entry={parsedEntry} className={className} />
+      return (
+        <EntryText
+          entry={parsedEntry}
+          className={className}
+          entryBefore={entryBefore}
+          entryAfter={entryAfter}
+        />
+      )
     case 'link':
-      return <EntryLink entry={parsedEntry} className={className} />
+      return (
+        <EntryLink
+          entry={parsedEntry}
+          className={className}
+          entryBefore={entryBefore}
+          entryAfter={entryAfter}
+        />
+      )
     case 'image':
       return (
         <EntryImage
           entry={parsedEntry}
           isFirstImage={isFirstImage}
           className={className}
+          entryBefore={entryBefore}
+          entryAfter={entryAfter}
         />
       )
     default:
@@ -171,35 +248,33 @@ function Entry({
   }
 }
 
+type GroupedEntries = Record<string, Entry[]>
+
 export default function PageTimeline() {
   // Sort entries by date in descending order
   const entries: Entry[] = (timelineData.entries || []).sort((a, b) => {
     const dateA = new Date(a.date || '1970-01-01').getTime()
     const dateB = new Date(b.date || '1970-01-01').getTime()
-    return dateA - dateB
+    return dateB - dateA
   })
 
   // Group entries by year and month
-  const groupedEntries = entries.reduce(
-    (acc: Record<string, Record<string, Entry[]>>, entry) => {
-      const date = new Date(entry.date || '1970-01-01')
-      const year = date.getFullYear().toString()
-      const month = date.getMonth().toString()
+  const groupedEntries = entries.reduce((acc: GroupedEntries, entry) => {
+    const date = new Date(entry.date || '1970-01-01')
+    const asIsoString = date.toISOString().split('T')[0]
 
-      if (!acc[year]) {
-        acc[year] = {}
-      }
+    if (!acc[asIsoString]) {
+      acc[asIsoString] = []
+    }
 
-      if (!acc[year][month]) {
-        acc[year][month] = []
-      }
+    acc[asIsoString].push(entry)
 
-      acc[year][month].push(entry)
+    return acc
+  }, {})
 
-      return acc
-    },
-    {}
-  )
+  let yearBefore: string | null = null
+  let monthBefore: string | null = null
+  let dayBefore: string | null = null
 
   return (
     <>
@@ -208,19 +283,35 @@ export default function PageTimeline() {
       </div>
 
       <div className='w-full'>
-        {Object.entries(groupedEntries).map(([year, months], index_year) => (
-          <div key={year}>
-            <Typo as='h2' className='mb-8 tab_content mx-auto'>
-              {year}
-            </Typo>
-            {Object.entries(months).map(([month, entries], index_month) => (
-              <div key={month} className='mb-8'>
-                <Typo as='h3' className='mb-4 tab_content mx-auto'>
-                  {getMonthName(parseInt(month))}
-                </Typo>
+        {Object.entries(groupedEntries).map(([key, entries], index_year) => {
+          const date = new Date(key)
+          const year = date.getFullYear().toString()
+          const month = date.getMonth().toString()
+          const day = date.getDate().toString()
+
+          const isNewYear = year !== yearBefore
+          const isNewMonth = month !== monthBefore
+
+          yearBefore = year
+          monthBefore = month
+          dayBefore = day
+
+          return (
+            <React.Fragment key={key}>
+              <Typo
+                as='h2'
+                className='!mb-0 pb-4 tab_content mx-auto font-bold space-x-2'
+              >
+                {isNewYear ? <span className='opacity-60'>{year}</span> : null}
+                {isNewMonth ? (
+                  <span>{getMonthName(parseInt(month))}</span>
+                ) : null}
+              </Typo>
+
+              {entries.length > 0 && (
                 <div
-                  key={month}
-                  className='mygrid gap-4 place-content-center place-items-center'
+                  key={`${year}-${month}-${day}`}
+                  className='mygrid gap-4 place-content-center place-items-center mb-4'
                 >
                   {entries.map((entry, index_entry) => {
                     const before = entries[index_entry - 1]
@@ -244,7 +335,7 @@ export default function PageTimeline() {
                       <Entry
                         className={cn(
                           'h-full w-full',
-                          'col-span-2 row-span-2 md:col-span-1 md:row-span-1 aspect-[1]',
+                          'col-span-1 row-span-1 md:col-span-1 md:row-span-1 aspect-[1]',
 
                           entryClone.imageAspectRatio === 2 &&
                             'col-span-2 md:col-span-2 aspect-[unset] md:aspect-[2] min-h-[50vw] md:min-h-full h-auto w-full md:h-full',
@@ -252,6 +343,7 @@ export default function PageTimeline() {
                             'col-span-1 row-span-2 md:col-span-1 md:row-span-2 aspect-[0.5]',
                           entryClone.displayAs !== 'image' &&
                             'col-span-2 row-span-1 max-w-[var(--content-box-width)] w-full aspect-[unset] md:w-[var(--content-box-width)] md:col-span-full h-auto md:h-auto',
+
                           entryClone.displayAs === 'image' &&
                             !isSurroundedByImages &&
                             'md:h-[var(--content-box-width)] md:col-span-full md:w-auto'
@@ -259,19 +351,19 @@ export default function PageTimeline() {
                         key={entryClone.date}
                         entry={entryClone}
                         isFirstImage={
-                          index_entry === 0 &&
-                          index_month === 0 &&
-                          index_year === 0 &&
-                          entryClone.displayAs === 'image'
+                          false
+                          // index_entry === 0 && entryClone.displayAs === 'image'
                         }
+                        entryBefore={before}
+                        entryAfter={after}
                       />
                     )
                   })}
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              )}
+            </React.Fragment>
+          )
+        })}
       </div>
     </>
   )
