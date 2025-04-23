c7135222a678740d1a108832cc65c038f5384910
Thomas Rosen <github@thomasrosen.me>
2025-04-23 17:21:37 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import nextMDX from '@next/mdx'
 import path from 'path'
-import recmaNextjsStaticProps from 'recma-nextjs-static-props'
-import { refractor } from 'refractor'
 // import refractor_bash from 'refractor/lang/bash'
 // import refractor_csv from 'refractor/lang/csv'
 // import refractor_excelFormula from 'refractor/lang/excel-formula'
@@ -9,21 +6,10 @@ import { refractor } from 'refractor'
 // import refractor_jsx from 'refractor/lang/jsx'
 import type { NextConfig } from 'next'
 // import recmaMdxImportMedia from 'recma-mdx-import-media'
-import remarkBreaks from 'remark-breaks'
-import remarkComment from 'remark-comment'
-import remarkFootnotesExtra from 'remark-footnotes-extra'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkGfm from 'remark-gfm'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import { fileURLToPath } from 'url'
 // import { rehypePreElements } from './src/lib/unified/rehypePreElements'
 // import recmaMdxChangeImports from 'recma-mdx-change-imports'
 // import withYaml from 'next-plugin-yaml'
-import recmaExportFilepath from 'recma-export-filepath'
-import recmaMdxEscapeMissingComponents from 'recma-mdx-escape-missing-components'
-import rehypeMdxTitle from 'rehype-mdx-title'
-import rehypePreLanguage from 'rehype-pre-language'
-import { remarkFootnoteReferences } from './src/lib/unified/remarkFootnoteReferences'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -79,7 +65,7 @@ let nextConfig: NextConfig = {
 
     // Add rule for XML/RSS files
     config.module.rules.push({
-      test: /\.(xml|rss)$/,
+      test: /\.(xml|rss|md|mdx)$/,
       use: {
         loader: 'raw-loader',
       },
@@ -144,7 +130,7 @@ let nextConfig: NextConfig = {
   },
 
   // Configure pageExtensions to include md and mdx
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
 
@@ -156,111 +142,13 @@ let nextConfig: NextConfig = {
 // nextConfig = withYaml(nextConfig)
 
 // Merge MDX config with Next.js config
+/*
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   // Optionally provide remark and rehype plugins
-  options: {
-    recmaPlugins: [
-      recmaMdxEscapeMissingComponents,
-      // recmaMdxChangeImports,
-      // recmaMdxImportMedia,
-      [
-        recmaExportFilepath,
-        {
-          absolute: true,
-        },
-      ],
-      recmaNextjsStaticProps,
-    ],
-    remarkPlugins: [
-      remarkComment,
-      remarkGfm,
-      remarkBreaks,
-      [remarkFootnotesExtra, { breakLink: true }],
-      remarkFootnoteReferences,
-      remarkFrontmatter,
-      [remarkMdxFrontmatter, { name: 'data' }],
-    ],
-    rehypePlugins: [
-      rehypePreLanguage,
-      rehypeMdxTitle,
-      // rehypePreElements,
-      // ['rehype-katex', { strict: true, throwOnError: true }]
-    ],
-    remarkRehypeOptions: {
-      allowDangerousHtml: true,
-      handlers: {
-        inlineCode: (state, node, parent) => {
-          // pass the value to the code element
-          return {
-            type: 'element',
-            tagName: 'code',
-            properties: {
-              value: node.value,
-            },
-            children: [
-              {
-                type: 'text',
-                value: node.value,
-              },
-            ],
-          }
-        },
-        code: (state, node, parent) => {
-          // instead of refractor, we could also use https://github.com/wooorm/starry-night
-          // starry-night is probably the best we can get, but it is more heavy.
-
-          let highlightTreeChildren = []
-          try {
-            const highlightTree = refractor.highlight(
-              node.value,
-              node.lang || 'txt'
-            )
-            highlightTreeChildren = highlightTree.children
-          } catch (error) {
-            console.error('ERROR_aeJKJvEI', error)
-
-            highlightTreeChildren = [
-              {
-                type: 'text',
-                value: node.value,
-              },
-            ]
-          }
-
-          // pass lang and value to the pre element
-          return {
-            type: 'element',
-            tagName: 'pre',
-            properties: {
-              lang: node.lang,
-              value: node.value,
-              className: `language-${node.lang}`,
-            },
-            children: highlightTreeChildren,
-          }
-        },
-      },
-      unknownHandler: (state, node, parent) => {
-        if (node.type === 'footnoteReferenceLink') {
-          return {
-            type: 'element',
-            tagName: 'a',
-            properties: {
-              'data-is-footnote': true,
-              'data-label': node.label,
-              href: node.url,
-            },
-            children: node.children,
-          }
-        }
-
-        return node
-      },
-    },
-  },
+  options: 
 })
+nextConfig = withMDX(nextConfig)
+*/
 
-const nextConfigWithMDX = withMDX(nextConfig)
-
-export default nextConfigWithMDX
+export default nextConfig
