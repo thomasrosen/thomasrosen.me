9f87613d4a549f944bf26c70bc4ed8111c709c01
Thomas Rosen <github@thomasrosen.me>
2025-04-24 01:09:06 +0200

    Update page.tsx

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -11,12 +11,12 @@ export default async function Page() {
         <Typo as='h2'>Playlists</Typo>
 
         <Typo as='p'>
-          You ever wanted to knwo what I'm listening to? Well, you can! I've
+          You ever wanted to know what I'm listening to? Well, you can! I've
           been keeping track of what I listen to since 2018.
         </Typo>
         <Typo as='p'>
           The following playlists are songs I liked or discovered in the month
-          they were created.
+          of that playlist.
         </Typo>
 
         <Link
