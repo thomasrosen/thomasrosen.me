27ceb52db14cf3a7ca38a31395537c333eec475b
Thomas Rosen <github@thomasrosen.me>
2025-04-23 18:42:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ let nextConfig: NextConfig = {
 
   output: 'standalone',
   outputFileTracingIncludes: {
-    '/*': ['src/data/blog/articles/**/*'],
+    '/*': ['src/data/**/*'],
   },
 
   webpack: (config, { isServer }) => {
