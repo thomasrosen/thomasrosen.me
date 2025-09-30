942db0f5a4ded8dbe58cbd3b2303a98ab379ab4c
thomasrosen <thomas.rosen@me.com>
2025-09-30 15:38:36 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
-        lg: 'h-12 rounded-full px-6 font-bold text-xl has-[>svg]:px-4',
+        lg: 'h-12 gap-3 rounded-full px-6 font-bold text-xl has-[>svg]:px-4',
         icon: 'size-9',
       },
     },
