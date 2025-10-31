0704a5bae30cb8b6ed6df19cf4b8bd325ab45a20
thomasrosen <thomas.rosen@me.com>
2025-10-31 14:37:51 +0100

    prevent modifiing the orginial object

--- src/lib/embedding/entryToSearchString.ts
+++ src/lib/embedding/entryToSearchString.ts
@@ -1,14 +1,19 @@
-export function entryToSearchString(data: any) {
-  data.id = undefined // this is not good in a search string for embeddings
-  data.image = undefined
-  data.imageAspectRatio = undefined
-  data.imageOrientation = undefined
-  data.audio = undefined
+export function entryToSearchString(data: any): string {
+  const clonedData = JSON.parse(JSON.stringify(data))
 
-  // delete all properties that are null or undefined
-  const cleanedData = Object.fromEntries(
-    Object.entries(data).filter(([_, v]) => v !== null && v !== undefined && v !== '')
-  )
+  let cleanedData = clonedData
+  if (typeof clonedData === 'object' && clonedData !== null) {
+    clonedData.id = undefined // this is not good in a search string for embeddings
+    clonedData.image = undefined
+    clonedData.imageAspectRatio = undefined
+    clonedData.imageOrientation = undefined
+    clonedData.audio = undefined
+
+    // delete all properties that are null or undefined
+    cleanedData = Object.fromEntries(
+      Object.entries(clonedData).filter(([_, v]) => v !== null && v !== undefined && v !== '')
+    )
+  }
 
   return JSON.stringify(cleanedData)
     .replace(/[(){}[\]"";]/g, ' ') // remove json-stringify characters
