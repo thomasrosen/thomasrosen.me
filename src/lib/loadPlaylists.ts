e9baf34645257b7a2a5c5d37cea428cc15ba124f
thomasrosen <thomas.rosen@me.com>
2025-11-03 03:21:51 +0100

    Update loadPlaylists.ts

--- src/lib/loadPlaylists.ts
+++ src/lib/loadPlaylists.ts
@@ -1,5 +1,3 @@
-import { simpify_music_tags } from './simpify_music_tags.mjs'
-
 export async function loadPlaylists() {
   try {
     const playlistData = await import('@/data/music/playlists.json')
@@ -23,7 +21,6 @@ export async function loadPlaylists() {
           return {
             ...playlist,
             coverphoto: coverphoto_src,
-            genres: playlist.genres.flatMap(simpify_music_tags),
           }
         })
     )
